6a2503be49694460396eef196ac1069ed3d9b9e9
MitziYolotzin <mitziulter@gmail.com>
2020-06-17 10:01:23 -0500

    Updates

---
+++ precache-manifest.b5e991227604a3cc58cf21676e02988a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5574a9ad43a70538794af9622f545c",
+    "url": "/project-react-api/index.html"
+  },
+  {
+    "revision": "67c945a4735547d93f04",
+    "url": "/project-react-api/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "f67330e41215140baf01",
+    "url": "/project-react-api/static/css/main.9ea86d8c.chunk.css"
+  },
+  {
+    "revision": "67c945a4735547d93f04",
+    "url": "/project-react-api/static/js/2.c3c5477a.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/project-react-api/static/js/2.c3c5477a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67330e41215140baf01",
+    "url": "/project-react-api/static/js/main.8a21ce7c.chunk.js"
+  },
+  {
+    "revision": "e9c7cb5f990a2c668bf5",
+    "url": "/project-react-api/static/js/runtime-main.380b3712.js"
+  },
+  {
+    "revision": "6058de967fa87c0ef32dbaa253eb2274",
+    "url": "/project-react-api/static/media/pokemon.6058de96.svg"
+  }
+]);
